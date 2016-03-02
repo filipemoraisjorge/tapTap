56a314a01a617741b672e6c183a96d6818ab6fa0
Filipe Jorge <filipemoraisjorge@gmail.com>
2016-03-02 18:36:31 +0000

    first commit

---
+++ desktop/src/org/academiadecodigo/bootcamp/filipejorge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.bootcamp.filipejorge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.bootcamp.filipejorge.TapTap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TapTap(), config);
+	}
+}
