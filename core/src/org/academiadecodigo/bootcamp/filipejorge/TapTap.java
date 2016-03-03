3c05a6ae19b507527d2745a1625de72eb96b3d6b
Filipe Jorge <filipemoraisjorge@gmail.com>
2016-03-03 03:02:01 +0000

    first tests

--- core/src/org/academiadecodigo/bootcamp/filipejorge/TapTap.java
+++ core/src/org/academiadecodigo/bootcamp/filipejorge/TapTap.java
@@ -3,26 +3,40 @@ package org.academiadecodigo.bootcamp.filipejorge;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 
 public class TapTap extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
+    private final float WIDTH = 800;
+    private final float HEIGHT = 480;
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+
+    private Circle markPlayer1;
+    private Circle markPlayer2;
+
+    @Override
+    public void create() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, WIDTH, HEIGHT);
+
+        markPlayer1.radius = 60;
+        markPlayer2.radius = 60;
+
+        Rectangle line = new Rectangle(WIDTH / 2, HEIGHT, HEIGHT, 5);
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.graphics.setContinuousRendering(false);
+        Gdx.graphics.requestRendering();
+        camera.update();
+    }
 }
--- desktop/src/org/academiadecodigo/bootcamp/filipejorge/desktop/DesktopLauncher.java
+++ desktop/src/org/academiadecodigo/bootcamp/filipejorge/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.academiadecodigo.bootcamp.filipejorge.TapTap;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TapTap(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "TapTap";
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new TapTap(), config);
+    }
 }
